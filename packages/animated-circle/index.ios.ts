7f20747580bafdeae85f7a839a4fa587dd858575
Brad Martin <bradwaynemartin@gmail.com>
2020-10-02 12:22:45 -0500

    closes #17

--- packages/animated-circle/index.ios.ts
+++ packages/animated-circle/index.ios.ts
@@ -1,9 +1,10 @@
 import { Color, Utils } from '@nativescript/core';
-import { AnimatedCircleCommon } from './common';
+import { AnimatedCircleCommon, barColorProperty, rimColorProperty, spinBarColorProperty } from './common';
 
 export class AnimatedCircle extends AnimatedCircleCommon {
 	private _ios: DRCircularProgressView;
 	private _label: UILabel;
+	private _text: string;
 
 	constructor() {
 		super();
@@ -33,6 +34,26 @@ export class AnimatedCircle extends AnimatedCircleCommon {
 		this._ios.bringSubviewToFront(this._label);
 	}
 
+	// @ts-ignore
+	get ios() {
+		return this._ios;
+	}
+
+	public spin() {}
+
+	public stopSpinning() {}
+
+	get spinBarColor(): string | UIColor {
+		return this._ios.alternativeColor;
+	}
+	set spinBarColor(value: string | UIColor) {
+		if (value instanceof UIColor) {
+			this._ios.alternativeColor = value;
+		} else if (typeof value === 'string') {
+			this._ios.alternativeColor = new Color(value).ios;
+		}
+	}
+
 	/**
 	 * The percentage value completed (whole number)
 	 */
@@ -51,6 +72,10 @@ export class AnimatedCircle extends AnimatedCircleCommon {
 		}
 	}
 
+	get barColor(): string | UIColor {
+		return this._ios.progressColor;
+	}
+
 	/**
 	 * The "remaining" circle color
 	 */
@@ -60,6 +85,38 @@ export class AnimatedCircle extends AnimatedCircleCommon {
 		} else if (typeof value === 'string') {
 			this._ios.alternativeColor = new Color(value).ios;
 		}
+	}
+
+	get rimColor(): string | UIColor {
+		return this._ios.alternativeColor;
+	}
+
+	/**
+	 * The "remaining" circle color
+	 */
+	[rimColorProperty.setNative](value: any) {
+		this._ios.alternativeColor = value.ios;
+	}
+
+	/**
+	 * The fill color of the percentage completed
+	 */
+	[barColorProperty.setNative](value: any) {
+		this._ios.progressColor = value.ios;
+	}
+
+	/**
+	 * The fill color of the percentage completed
+	 */
+	[spinBarColorProperty.setNative](value: any) {
+		this._ios.alternativeColor = value.ios;
+	}
+
+	/**
+	 * The draw direction for the filled circle
+	 */
+	set clockwise(value: boolean) {
+		this._ios.clockwise = value;
 	}
 
 	/**
