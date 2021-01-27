991d31a1ace696078f76b302839f8372b27c60e3
triniwiz <fortune.osei@yahoo.com>
2021-01-27 19:19:51 -0400

    fix(android):  Android 11 camera support

--- packages/camera/index.android.ts
+++ packages/camera/index.android.ts
@@ -80,81 +80,81 @@ export let takePicture = function (options?): Promise<any> {
 				takePictureIntent.putExtra('android.intent.extras.CAMERA_FACING', android.hardware.Camera.CameraInfo.CAMERA_FACING_BACK);
 			}
 
-			if (takePictureIntent.resolveActivity(Utils.android.getApplicationContext().getPackageManager()) != null) {
-				// Remove previous listeners if any
-				Application.android.off('activityResult');
+			// Remove previous listeners if any
+			Application.android.off('activityResult');
 
-				Application.android.on('activityResult', (args) => {
-					const requestCode = args.requestCode;
-					const resultCode = args.resultCode;
+			Application.android.on('activityResult', (args) => {
+				const requestCode = args.requestCode;
+				const resultCode = args.resultCode;
 
-					if (requestCode === REQUEST_IMAGE_CAPTURE && resultCode === android.app.Activity.RESULT_OK) {
-						const currentTimeMillis = java.lang.Long.valueOf(java.lang.System.currentTimeMillis());
-						const values = new android.content.ContentValues();
-						values.put(android.provider.MediaStore.MediaColumns.DISPLAY_NAME, fileName);
-						values.put((android as any).provider.MediaStore.Images.Media.DATE_ADDED, currentTimeMillis);
-						values.put(android.provider.MediaStore.MediaColumns.MIME_TYPE, 'image/*');
-						if (sdkVersionInt >= 29) {
-							values.put(android.provider.MediaStore.MediaColumns.RELATIVE_PATH, android.os.Environment.DIRECTORY_DCIM);
-							values.put(android.provider.MediaStore.MediaColumns.IS_PENDING, java.lang.Integer.valueOf(1));
-							values.put((android as any).provider.MediaStore.Images.Media.DATE_TAKEN, currentTimeMillis);
-						}
+				if (requestCode === REQUEST_IMAGE_CAPTURE && resultCode === android.app.Activity.RESULT_OK) {
+					const currentTimeMillis = java.lang.Integer.valueOf(java.lang.System.currentTimeMillis());
+					const values = new android.content.ContentValues();
+					values.put(android.provider.MediaStore.MediaColumns.DISPLAY_NAME, fileName);
+					values.put(android.provider.MediaStore.MediaColumns.DATE_ADDED, currentTimeMillis);
+					values.put(android.provider.MediaStore.MediaColumns.DATE_MODIFIED, currentTimeMillis);
+					values.put(android.provider.MediaStore.MediaColumns.MIME_TYPE, 'image/*');
 
-						const uri = Utils.android.getApplicationContext().getContentResolver().insert(android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
-
-						if (saveToGallery) {
-							const fos: java.io.FileOutputStream = Utils.android.getApplicationContext().getContentResolver().openOutputStream(uri);
-							const fis = new java.io.FileInputStream(nativeFile);
-							try {
-								(org as any).nativescript.plugins.camera.Utils.copy(fis, fos);
-								if (sdkVersionInt >= 29) {
-									values.clear();
-									values.put((android as any).provider.MediaStore.Video.Media.IS_PENDING, java.lang.Integer.valueOf(0));
-									Utils.android.getApplicationContext().contentResolver.update(uri, values, null, null);
-								}
-							} catch (e) {
-								reject(e);
-							}
-						}
-
-						let exif = new android.media.ExifInterface(picturePath);
-						let orientation = exif.getAttributeInt(android.media.ExifInterface.TAG_ORIENTATION, android.media.ExifInterface.ORIENTATION_NORMAL);
-
-						if (orientation === android.media.ExifInterface.ORIENTATION_ROTATE_90) {
-							rotateBitmap(picturePath, 90);
-						} else if (orientation === android.media.ExifInterface.ORIENTATION_ROTATE_180) {
-							rotateBitmap(picturePath, 180);
-						} else if (orientation === android.media.ExifInterface.ORIENTATION_ROTATE_270) {
-							rotateBitmap(picturePath, 270);
-						}
-
-						if (shouldKeepAspectRatio) {
-							let pictureWidth = exif.getAttributeInt(android.media.ExifInterface.TAG_IMAGE_WIDTH, 0);
-							let pictureHeight = exif.getAttributeInt(android.media.ExifInterface.TAG_IMAGE_LENGTH, 0);
-							let isPictureLandscape = pictureWidth > pictureHeight;
-							let areOptionsLandscape = reqWidth > reqHeight;
-							if (isPictureLandscape !== areOptionsLandscape) {
-								let oldReqWidth = reqWidth;
-								reqWidth = reqHeight;
-								reqHeight = oldReqWidth;
-							}
-						}
-
-						let asset = new ImageAsset(picturePath);
-						asset.options = {
-							width: reqWidth,
-							height: reqHeight,
-							keepAspectRatio: shouldKeepAspectRatio,
-						};
-						resolve(asset);
-					} else if (resultCode === android.app.Activity.RESULT_CANCELED) {
-						// User cancelled the image capture
-						reject(new Error('cancelled'));
+					if (sdkVersionInt >= 29) {
+						values.put(android.provider.MediaStore.MediaColumns.RELATIVE_PATH, android.os.Environment.DIRECTORY_DCIM);
+						values.put(android.provider.MediaStore.MediaColumns.IS_PENDING, java.lang.Integer.valueOf(1));
+						values.put(android.provider.MediaStore.MediaColumns.DATE_TAKEN, currentTimeMillis);
 					}
-				});
 
-				(Application.android.foregroundActivity || Application.android.startActivity).startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-			}
+					const uri = Utils.android.getApplicationContext().getContentResolver().insert(android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+
+					if (saveToGallery) {
+						const fos: java.io.FileOutputStream = Utils.android.getApplicationContext().getContentResolver().openOutputStream(uri);
+						const fis = new java.io.FileInputStream(nativeFile);
+						try {
+							(org as any).nativescript.plugins.camera.Utils.copy(fis, fos);
+							if (sdkVersionInt >= 29) {
+								values.clear();
+								values.put((android as any).provider.MediaStore.Video.Media.IS_PENDING, java.lang.Integer.valueOf(0));
+								Utils.android.getApplicationContext().getContentResolver().update(uri, values, null, null);
+							}
+						} catch (e) {
+							reject(e);
+						}
+					}
+
+					let exif = new android.media.ExifInterface(picturePath);
+					let orientation = exif.getAttributeInt(android.media.ExifInterface.TAG_ORIENTATION, android.media.ExifInterface.ORIENTATION_NORMAL);
+
+					if (orientation === android.media.ExifInterface.ORIENTATION_ROTATE_90) {
+						rotateBitmap(picturePath, 90);
+					} else if (orientation === android.media.ExifInterface.ORIENTATION_ROTATE_180) {
+						rotateBitmap(picturePath, 180);
+					} else if (orientation === android.media.ExifInterface.ORIENTATION_ROTATE_270) {
+						rotateBitmap(picturePath, 270);
+					}
+
+					if (shouldKeepAspectRatio) {
+						let pictureWidth = exif.getAttributeInt(android.media.ExifInterface.TAG_IMAGE_WIDTH, 0);
+						let pictureHeight = exif.getAttributeInt(android.media.ExifInterface.TAG_IMAGE_LENGTH, 0);
+						let isPictureLandscape = pictureWidth > pictureHeight;
+						let areOptionsLandscape = reqWidth > reqHeight;
+						if (isPictureLandscape !== areOptionsLandscape) {
+							let oldReqWidth = reqWidth;
+							reqWidth = reqHeight;
+							reqHeight = oldReqWidth;
+						}
+					}
+
+					let asset = new ImageAsset(picturePath);
+					asset.options = {
+						width: reqWidth,
+						height: reqHeight,
+						keepAspectRatio: shouldKeepAspectRatio,
+					};
+					resolve(asset);
+				} else if (resultCode === android.app.Activity.RESULT_CANCELED) {
+					// User cancelled the image capture
+					reject(new Error('cancelled'));
+				}
+			});
+
+			(Application.android.foregroundActivity || Application.android.startActivity).startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
 		} catch (e) {
 			if (reject) {
 				reject(e);
