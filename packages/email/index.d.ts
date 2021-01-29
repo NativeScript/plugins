e9a1662bd327ff3eb3820620cf9b488c07d89752
Nathan Walker <walkerrunpdx@gmail.com>
2021-01-28 20:12:40 -0800

    release(email): 2.0.2 - ios viewcontroller control and improvements

--- packages/email/index.d.ts
+++ packages/email/index.d.ts
@@ -1,72 +1,78 @@
 export interface Attachment {
-  /**
-   * The name used for the attachment.
-   * Example:
-   *
-   *   fileName: 'Cute-Kitten.png'
-   */
-  fileName: string;
+	/**
+	 * The name used for the attachment.
+	 * Example:
+	 *
+	 *   fileName: 'Cute-Kitten.png'
+	 */
+	fileName: string;
 
-  /**
-   * There are various ways to use the path param:
-   *
-   *   - base64 encoded: 'base64://iVBORw..XYZ'
-   *   - local app folder 'file://..
-   *   - anywhere on the device: 'file:///..'
-   *   - or '/some/path/to/file.png'
-   */
-  path: string;
+	/**
+	 * There are various ways to use the path param:
+	 *
+	 *   - base64 encoded: 'base64://iVBORw..XYZ'
+	 *   - local app folder 'file://..
+	 *   - anywhere on the device: 'file:///..'
+	 *   - or '/some/path/to/file.png'
+	 */
+	path: string;
 
-  /**
-   * Used to help the iOS figure out how to send the file (not used on Android).
-   * Example:
-   *
-   *   mimeType: 'image/png'
-   */
-  mimeType: string;
+	/**
+	 * Used to help the iOS figure out how to send the file (not used on Android).
+	 * Example:
+	 *
+	 *   mimeType: 'image/png'
+	 */
+	mimeType: string;
 }
 
 /**
  * The options object passed into the compose function.
  */
 export interface ComposeOptions {
-  /**
-   * The subject of your email.
-   */
-  subject?: string;
+	/**
+	 * The subject of your email.
+	 */
+	subject?: string;
 
-  /**
-   * The plugin will automatically handle plain and html email content.
-   */
-  body?: string;
+	/**
+	 * The plugin will automatically handle plain and html email content.
+	 */
+	body?: string;
 
-  /**
-   * A string array of email addresses.
-   * Known issue: on Android only the first item in the array is added.
-   */
-  to?: string[];
+	/**
+	 * A string array of email addresses.
+	 * Known issue: on Android only the first item in the array is added.
+	 */
+	to?: string[];
 
-  /**
-   * A string array of email addresses.
-   * Known issue: on Android only the first item in the array is added.
-   */
-  cc?: string[];
+	/**
+	 * A string array of email addresses.
+	 * Known issue: on Android only the first item in the array is added.
+	 */
+	cc?: string[];
 
-  /**
-   * A string array of email addresses.
-   * Known issue: on Android only the first item in the array is added.
-   */
-  bcc?: string[];
+	/**
+	 * A string array of email addresses.
+	 * Known issue: on Android only the first item in the array is added.
+	 */
+	bcc?: string[];
 
-  /**
-   * An optional Array of attachments.
-   */
-  attachments?: Array<Attachment>;
+	/**
+	 * An optional Array of attachments.
+	 */
+	attachments?: Array<Attachment>;
 
-  /**
-   * @deprecated No longer used, but keeping it around to notify you.
-   */
-  appPickerTitle?: string;
+	/**
+	 * An optional iOS View Controller to open compose from
+	 * IOS ONLY
+	 */
+	iosViewController?: any;
+
+	/**
+	 * @deprecated No longer used, but keeping it around to notify you.
+	 */
+	appPickerTitle?: string;
 }
 
 /**
