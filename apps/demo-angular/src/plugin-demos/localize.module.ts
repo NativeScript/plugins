685f7be7655a0e7df63f72237e39732b822af186
Nathan Walker <walkerrunpdx@gmail.com>
2021-02-21 12:39:10 -0800

    chore: workspace migration plugin-tools 1.1.0

--- apps/demo-angular/src/plugin-demos/localize.module.ts
+++ apps/demo-angular/src/plugin-demos/localize.module.ts
@@ -1,9 +1,10 @@
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { NativeScriptCommonModule, NativeScriptRouterModule } from '@nativescript/angular';
+import { NativeScriptLocalizeModule } from '@nativescript/localize/angular';
 import { LocalizeComponent } from './localize.component';
 
 @NgModule({
-	imports: [NativeScriptCommonModule, NativeScriptRouterModule.forChild([{ path: '', component: LocalizeComponent }])],
+	imports: [NativeScriptCommonModule, NativeScriptRouterModule.forChild([{ path: '', component: LocalizeComponent }]), NativeScriptLocalizeModule],
 	declarations: [LocalizeComponent],
 	schemas: [NO_ERRORS_SCHEMA],
 })
