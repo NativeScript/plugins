8e9756b79bee3dab1fb3f758edde774aa983c5d1
JamesCodesThings <james.macmillan@engineius.co.uk>
2021-06-02 15:59:30 +0100

    fix(directions): Add option to use Universal URL Scheme for Google Maps
    - Found that google maps does not load correctly first time when using lat & lng.
    - When hacking around the module found using the updated Universal URL syntax works.
    - Added an option to use the Universal URL Scheme when google maps is chosen.
    
    Squash Of:
    
    commit b258cb907eda0fcd3c815572b962290ab123d4ca
    Author: JamesCodesThings <james.macmillan@engineius.co.uk>
    Date:   Wed Jun 2 15:56:58 2021 +0100
    
        make sure it works in the same way on android
    
    commit 95797840d35d8e52e6bbc9e96b49a8b41d42599a
    Author: JamesCodesThings <james.macmillan@engineius.co.uk>
    Date:   Wed Jun 2 15:16:00 2021 +0100
    
        Fix up Universal URL Scheme
    
    commit 48a1c5d05f53c5601af7c4f2ed0aff67311cc4c2
    Author: JamesCodesThings <james.macmillan@engineius.co.uk>
    Date:   Wed Jun 2 11:26:10 2021 +0100
    
        update demo-angular to something useful
    
    commit fd9fd94dbfc77b02d86f0609defd9e89785b1577
    Author: JamesCodesThings <james.macmillan@engineius.co.uk>
    Date:   Wed Jun 2 09:40:36 2021 +0100
    
        stash
    
    commit 14966eade382f94fb8dac7611fb95b40936b2a63
    Author: JamesCodesThings <james.macmillan@engineius.co.uk>
    Date:   Tue Jun 1 10:50:13 2021 +0100
    
        chore(directions): clean up index.ios.ts
    
        - Reduce nesting in the navigate method
        - Introduce optional chaining to reduce the if condition length/complexity.

--- tools/demo/directions/index.ts
+++ tools/demo/directions/index.ts
@@ -1,94 +1,134 @@
 import { DemoSharedBase } from '../utils';
-import { Directions } from '@nativescript/directions';
+import { Directions, NavigateToOptions } from '@nativescript/directions';
 
 export class DemoSharedDirections extends DemoSharedBase {
 	private directions: Directions;
 
-  constructor() {
-    super();
-    this.directions = new Directions();
-  }
+	constructor() {
+		super();
+		this.directions = new Directions();
+	}
 
-  doCheckAvailable() {
-    this.directions.available().then((avail) => {
-      console.log("Avail? " + avail);
-      alert(avail);
-    }, (err) => {
-      alert(err);
-    });
-  }
+	async doCheckAvailable() {
+		try {
+			const result = await this.directions.available();
+			const message = `Directions Available: ${result}`;
+			console.log(message);
+			alert(message);
+		} catch (error) {
+			console.error('Error:', error);
+			alert(`Error: ${error?.message}`);
+		}
+	}
 
-  doCurrentLocationToAddress() {
-    this.directions.navigate({
-      to: {
-        address: "Hof der Kolommen 34, Amersfoort, Netherlands"
-      },
-      type: "transit"
-    }).then(() => {
-      console.log("Current location to address directions launched!");
-    }, (err) => {
-      alert(err);
-    });
-  }
+	/**
+	 * Tests navigation via the input options
+	 * @param options
+	 */
+	async doNavigate(options: NavigateToOptions = { to: { address: '' } }) {
+		try {
+			await this.directions.navigate(options);
+			console.log('Navigate call complete');
+		} catch (error) {
+			console.error('Navigate Error', error);
+			alert(`Navigate error ${error?.message}`);
+		}
+	}
 
-  doAddressToAddressToAddress() {
-    this.directions.navigate({
-      from: {
-        address: "Hof der Kolommen 34, Amersfoort, Netherlands"
-      },
-      type: "walking",
-      to: [
-        {
-          address: "Middenstraat 21, Loppersum, Netherlands"
-        },
-        {
-          address: "Aak 98, Wieringerwerf, Netherlands"
-        }],
-      ios: {
-        allowGoogleMapsWeb: true
-      }
-    }).then(() => {
-      console.log("Address to address directions launched!");
-    }, (err) => {
-      alert(err);
-    });
-  }
+	doCurrentLocationToAddress() {
+		this.directions
+			.navigate({
+				to: {
+					address: 'Hof der Kolommen 34, Amersfoort, Netherlands',
+				},
+				type: 'transit',
+			})
+			.then(
+				() => {
+					console.log('Current location to address directions launched!');
+				},
+				(err) => {
+					alert(err);
+				}
+			);
+	}
 
-  doAddressToCoordinate() {
-    this.directions.navigate({
-      from: {
-        address: "Hof der Kolommen 34, Amersfoort, Netherlands"
-      },
-      to: [{
-        lat: 52.215987,
-        lng: 5.282764
-      }],
-      type: "walking",
-      ios: {
-        allowGoogleMapsWeb: true,
-        preferGoogleMaps: true
-      }
-    }).then(() => {
-      console.log("Address to coord directions launched!");
-    }, (err) => {
-      alert(err);
-    });
-  }
+	doAddressToAddressToAddress() {
+		this.directions
+			.navigate({
+				from: {
+					address: 'Hof der Kolommen 34, Amersfoort, Netherlands',
+				},
+				type: 'walking',
+				to: [
+					{
+						address: 'Middenstraat 21, Loppersum, Netherlands',
+					},
+					{
+						address: 'Aak 98, Wieringerwerf, Netherlands',
+					},
+				],
+				ios: {
+					allowGoogleMapsWeb: true,
+				},
+			})
+			.then(
+				() => {
+					console.log('Address to address directions launched!');
+				},
+				(err) => {
+					alert(err);
+				}
+			);
+	}
 
-  doCoordinateToCoordinate() {
-    this.directions.navigate({
-      from: {
-        lat: 52.215987,
-        lng: 5.282764
-      },
-      to: {
-        lat: 52.515987,
-        lng: 5.382764
-      }
-    }).then(() => {
-      console.log("Coord to coord directions launched!");
-    }, (err) => {
-      alert(err);
-    });
-  }
+	doAddressToCoordinate() {
+		this.directions
+			.navigate({
+				from: {
+					address: 'Hof der Kolommen 34, Amersfoort, Netherlands',
+				},
+				to: [
+					{
+						lat: 52.215987,
+						lng: 5.282764,
+					},
+				],
+				type: 'walking',
+				ios: {
+					allowGoogleMapsWeb: true,
+					preferGoogleMaps: true,
+				},
+			})
+			.then(
+				() => {
+					console.log('Address to coord directions launched!');
+				},
+				(err) => {
+					alert(err);
+				}
+			);
+	}
+
+	doCoordinateToCoordinate() {
+		this.directions
+			.navigate({
+				from: {
+					lat: 52.215987,
+					lng: 5.282764,
+				},
+				to: {
+					lat: 52.515987,
+					lng: 5.382764,
+				},
+			})
+			.then(
+				() => {
+					console.log('Coord to coord directions launched!');
+				},
+				(err) => {
+					alert(err);
+				}
+			);
+	}
 }
