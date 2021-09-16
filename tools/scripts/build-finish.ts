41577622fffd45053e3039d4c8f53dcb004d6d9a
Nathan Walker <walkerrunpdx@gmail.com>
2021-09-15 20:31:51 -0700

    chore: plugin-tools 2.2.1

--- tools/scripts/build-finish.ts
+++ tools/scripts/build-finish.ts
@@ -11,43 +11,47 @@ const cmdArgs = process.argv.slice(2);
 const packageName = cmdArgs[0];
 const publish = cmdArgs[1] === 'publish';
 
-console.log(`Building ${npmScope}/${packageName}...${publish ? 'and publishing.' : ''}`);
+const packagePath = path.join('packages', packageName, 'package.json');
+const packageJson = JSON.parse(fs.readFileSync(packagePath));
+const npmPackageName = packageJson.name;
+console.log(`Building ${npmPackageName}...${publish ? 'and publishing.' : ''}`);
 
 // build angular package
 function buildAngular() {
-	ngPackage
-		.ngPackagr()
-		.forProject(path.join('packages', packageName, 'angular', 'package.json'))
-		.withTsConfig(path.join('packages', packageName, 'angular', 'tsconfig.angular.json'))
-		.build()
-		.then(() => {
-			copyAngularDist();
-		})
-		.catch((error) => {
-			console.error(error);
-			process.exit(1);
-		});
+  ngPackage
+    .ngPackagr()
+    .forProject(path.join('packages', packageName, 'angular', 'package.json'))
+    .withTsConfig(path.join('packages', packageName, 'angular', 'tsconfig.angular.json'))
+    .build()
+    .then(() => {
+      copyAngularDist();
+    })
+    .catch((error) => {
+      console.error(error);
+      process.exit(1);
+    });
 }
 
 // copy angular ng-packagr output to dist/packages/{name}
 function copyAngularDist() {
-	fs.copy(path.join('packages', packageName, 'angular', 'dist'), path.join('dist', 'packages', packageName, 'angular'))
-		.then(() => {
-			console.log(`${packageName} angular built successfully.`);
-			finishPreparation();
-		})
-		.catch((err) => console.error(err));
+  fs.copy(path.join('packages', packageName, 'angular', 'dist'), path.join('dist', 'packages', packageName, 'angular'))
+    .then(() => {
+      console.log(`${npmPackageName} angular built successfully.`);
+      // buildNativeSrc();
+      finishPreparation();
+    })
+    .catch((err) => console.error(err));
 }
 
 function finishPreparation() {
-	fs.copy(path.join('tools', 'assets', 'publishing'), path.join('dist', 'packages', packageName))
-		.then(() => console.log(`${npmScope}/${packageName} ready to publish.`))
-		.catch((err) => console.error(err));
+  fs.copy(path.join('tools', 'assets', 'publishing'), path.join('dist', 'packages', packageName))
+    .then(() => console.log(`${npmPackageName} ready to publish.`))
+    .catch((err) => console.error(err));
 }
 
 if (fs.existsSync(path.join(rootDir, 'packages', packageName, 'angular'))) {
-	// package has angular specific src, build it first
-	buildAngular();
+  // package has angular specific src, build it first
+  buildAngular();
 } else {
-	finishPreparation();
+  finishPreparation();
 }
