c4a66e08b537d6f44d45b413a920e5c6df7fddaa
Nathan Walker <walkerrunpdx@gmail.com>
2021-10-07 14:37:57 -0700

    fix(picker): listview item template handling for angular

--- packages/picker/angular/picker.directive.ts
+++ packages/picker/angular/picker.directive.ts
@@ -1,44 +1,63 @@
-import { ChangeDetectionStrategy, Component, ElementRef, forwardRef, IterableDiffers, AfterContentInit, OnDestroy, NgZone } from '@angular/core';
-import { PickerField } from '@nativescript/picker';
+// import { ChangeDetectionStrategy, Component, ElementRef, forwardRef, IterableDiffers, AfterContentInit, OnDestroy, NgZone } from '@angular/core';
+// import { PickerField } from '@nativescript/picker';
+// import { PickerValueAccessor } from './picker.accessors';
+// import { EventData, View } from '@nativescript/core';
+
+// @Component({
+// 	selector: 'PickerField',
+// 	template: ` <DetachedContainer>
+// 		<Placeholder #loader></Placeholder>
+// 	</DetachedContainer>`,
+// 	changeDetection: ChangeDetectionStrategy.OnPush,
+// })
+// export class PickerFieldComponent implements AfterContentInit {
+// 	private _className: string;
+
+// 	public get nativeElement(): PickerField {
+// 		return this.templatedItemsView;
+// 	}
+
+// 	public get className(): string {
+// 		return this._className;
+// 	}
+
+// 	protected templatedItemsView: PickerField;
+
+// 	constructor(_elementRef: ElementRef, _iterableDiffers: IterableDiffers, _ngZone: NgZone) {
+// 		_elementRef.nativeElement.on('loaded', (ev: EventData) => {
+// 			this.nativeElement.on('classNameChange', this.onClassNameChange.bind(this));
+// 		});
+// 	}
+
+// 	ngOnDestroy() {
+// 		if (this.nativeElement) {
+// 			this.nativeElement.off('classNameChange', this.onClassNameChange.bind(this));
+// 		}
+// 	}
+
+// 	private onClassNameChange(args) {
+// 		this._className = (<View>args.object).className;
+// 	}
+// }
+
+
+
+
+import { AfterContentInit, ChangeDetectionStrategy, Component, ElementRef, forwardRef, IterableDiffers, NgZone } from '@angular/core';
+import { ListViewComponent, TEMPLATED_ITEMS_COMPONENT } from '@nativescript/angular';
 import { PickerValueAccessor } from './picker.accessors';
-import { View } from '@nativescript/core';
 
 @Component({
-	selector: 'PickerField',
-	template: ` <DetachedContainer>
-		<Placeholder #loader></Placeholder>
-	</DetachedContainer>`,
-	changeDetection: ChangeDetectionStrategy.OnPush,
+  selector: 'PickerField',
+  template: `<DetachedContainer>
+    <ng-container #loader></ng-container>
+  </DetachedContainer>`,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [{ provide: TEMPLATED_ITEMS_COMPONENT, useExisting: forwardRef(() => PickerFieldComponent) }],
 })
-export class PickerFieldComponent implements AfterContentInit {
-	private _className: string;
-
-	public get nativeElement(): PickerField {
-		return this.templatedItemsView;
-	}
-
-	public get className(): string {
-		return this._className;
-	}
-
-	protected templatedItemsView: PickerField;
-
-	constructor(_elementRef: ElementRef, _iterableDiffers: IterableDiffers, _ngZone: NgZone) {
-		
-	}
-
-	ngAfterContentInit() {
-		this.nativeElement.on('classNameChange', this.onClassNameChange.bind(this));
-	}
-
-	ngOnDestroy() {
-		if (this.nativeElement) {
-			this.nativeElement.off('classNameChange', this.onClassNameChange.bind(this));
-		}
-	}
-
-	private onClassNameChange(args) {
-		this._className = (<View>args.object).className;
+export class PickerFieldComponent extends ListViewComponent implements AfterContentInit {
+	constructor(_elementRef: ElementRef, _iterableDiffers: IterableDiffers, zone: NgZone) {
+		super(_elementRef, _iterableDiffers, zone);
 	}
 }
 
