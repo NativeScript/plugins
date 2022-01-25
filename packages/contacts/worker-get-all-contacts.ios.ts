42ab345a9e28c7f5063002c20c89f8bfc771d627
Igor Randjelovic <rigor789@gmail.com>
2022-01-25 18:09:26 +0100

    chore: cleanup contact workers

--- packages/contacts/worker-get-all-contacts.ios.ts
+++ packages/contacts/worker-get-all-contacts.ios.ts
@@ -1,57 +1,87 @@
-require('@nativescript/core/globals')
-var Contact = require("./models/contact").Contact;
+import '@nativescript/core/globals';
+
+import { Contact } from './models/contact';
 
 /* pass debug messages to main thread since web workers do not have console access */
 // function console_log(msg) { postMessage({ type: 'debug', message: msg }); }
 // function console_dump(msg) { postMessage({ type: 'dump', message: msg }); }
 
 self.onmessage = function (event) {
-  contactFields = event.data.contactFields || [
-		'name',
-		'organization',
-		'nickname',
-		'notes',
-		'photo',
-		'urls',
-		'phoneNumbers',
-		'emailAddresses',
-		'postalAddresses',
-	];
-  
-  var keysToFetch = [];  // All Properties that we are using in the Model
-  if (contactFields.indexOf('name') > -1) { 
-      keysToFetch.push(
-          "givenName", "familyName", "middleName", "namePrefix", "nameSuffix", 
-          "phoneticGivenName", "phoneticMiddleName", "phoneticFamilyName"
-      );
-  }
+	// prettier-ignore
+	const contactFields = event.data.contactFields || [
+    'name',
+    'organization',
+    'nickname',
+    'notes',
+    'photo',
+    'urls',
+    'phoneNumbers',
+    'emailAddresses',
+    'postalAddresses'
+  ];
 
-  if (contactFields.indexOf('organization') > -1) { keysToFetch.push("jobTitle", "departmentName", "organizationName"); }
-  if (contactFields.indexOf('nickname') > -1) { keysToFetch.push("nickname"); }
-  if (contactFields.indexOf('notes') > -1) { keysToFetch.push("note"); }
-  if (contactFields.indexOf('photo') > -1) { keysToFetch.push("imageData", "imageDataAvailable"); }
-  if (contactFields.indexOf('phoneNumbers') > -1) { keysToFetch.push("phoneNumbers"); }
-  if (contactFields.indexOf('emailAddresses') > -1) { keysToFetch.push("emailAddresses"); }
-  if (contactFields.indexOf('postalAddresses') > -1) { keysToFetch.push("postalAddresses"); }
-  if (contactFields.indexOf('urlAddresses') > -1) { keysToFetch.push("urlAddresses"); }
+	const keysToFetch = []; // All Properties that we are using in the Model
+	if (contactFields.indexOf('name') > -1) {
+		// prettier-ignore
+		keysToFetch.push(
+      'givenName',
+      'familyName',
+      'middleName',
+      'namePrefix',
+      'nameSuffix',
+      'phoneticGivenName',
+      'phoneticMiddleName',
+      'phoneticFamilyName'
+    );
+	}
 
-  var store = new CNContactStore(),
-  error,
-  fetch = CNContactFetchRequest.alloc().initWithKeysToFetch(keysToFetch),
-  cts = [],
-  nativeMutableArray = NSMutableArray.alloc().init();
-  
-  fetch.unifyResults = true;
-  fetch.predicate = null;
-  
-  store.enumerateContactsWithFetchRequestErrorUsingBlock(fetch, error, function(c,s){
-    nativeMutableArray.addObject(c);
-    var contactModel = new Contact();
-    contactModel.initializeFromNative(c,contactFields);
-    cts.push(contactModel);
-  });
-  
-  if(error) { postMessage({ type: 'error', message: error }); }
-  if(cts.length > 0) { postMessage({ type: 'result', message: { data: cts, response: "fetch" }}); }
-  else { postMessage({ type: 'result', message: { data: null, response: "fetch" }}); }
-}
+	if (contactFields.indexOf('organization') > -1) {
+		keysToFetch.push('jobTitle', 'departmentName', 'organizationName');
+	}
+	if (contactFields.indexOf('nickname') > -1) {
+		keysToFetch.push('nickname');
+	}
+	if (contactFields.indexOf('notes') > -1) {
+		keysToFetch.push('note');
+	}
+	if (contactFields.indexOf('photo') > -1) {
+		keysToFetch.push('imageData', 'imageDataAvailable');
+	}
+	if (contactFields.indexOf('phoneNumbers') > -1) {
+		keysToFetch.push('phoneNumbers');
+	}
+	if (contactFields.indexOf('emailAddresses') > -1) {
+		keysToFetch.push('emailAddresses');
+	}
+	if (contactFields.indexOf('postalAddresses') > -1) {
+		keysToFetch.push('postalAddresses');
+	}
+	if (contactFields.indexOf('urlAddresses') > -1) {
+		keysToFetch.push('urlAddresses');
+	}
+
+	const store = new CNContactStore();
+	let error: any;
+	let fetch = CNContactFetchRequest.alloc().initWithKeysToFetch(keysToFetch);
+	let cts = [];
+	let nativeMutableArray = NSMutableArray.alloc().init();
+
+	fetch.unifyResults = true;
+	fetch.predicate = null;
+
+	store.enumerateContactsWithFetchRequestErrorUsingBlock(fetch, error, function (c, s) {
+		nativeMutableArray.addObject(c);
+		const contactModel = new Contact();
+		contactModel.initializeFromNative(c, contactFields);
+		cts.push(contactModel);
+	});
+
+	if (error) {
+		postMessage({ type: 'error', message: error });
+	}
+	if (cts.length > 0) {
+		postMessage({ type: 'result', message: { data: cts, response: 'fetch' } });
+	} else {
+		postMessage({ type: 'result', message: { data: null, response: 'fetch' } });
+	}
+};
