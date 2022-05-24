6c1faadcce9879992f2fbddf3b29f9a1e89442fe
Nathan Walker <walkerrunpdx@gmail.com>
2022-05-23 17:48:50 -0700

    feat(ionic-portals): pub/sub for communication

--- packages/ionic-portals/index.d.ts
+++ packages/ionic-portals/index.d.ts
@@ -1,6 +1,6 @@
-import { IonicPortalCommon } from './common';
+import { IonicPortalCommon, IonicPortalManagerCommon } from './common';
 
-export declare class IonicPortalManager {
+export declare class IonicPortalManager extends IonicPortalManagerCommon {
 	/**
 	 * Register Portals when your app boots
 	 * https://ionic.io/docs/portals/getting-started/guide#configure
@@ -8,14 +8,34 @@ export declare class IonicPortalManager {
 	 */
 	static register(apiKey: string): void;
 	/**
-	 * Create a Portal
-	 * @param portalId The portal id to register
-	 * @param startDir Set the web application directory. By default it will look for a folder named the same as the portalId as the location of the web assets.
-	 * @returns platform native Portal
-	 * For example if you created a Portal with id 'webPortal':
-	 * iOS: App_Resources/iOS/webPortal
-	 * Android: App_Resources/Android/src/main/asssets/webPortal
+	 * Used to set the initial context of any portal id before the portal is shown
+	 * @param id portal id
+	 * @param initialContext data provided as the initial context to the portal
 	 */
-	static create(portalId: string, startDir?: string, plugins?: Array<string> /* Android only - if using non-official Capacitor plugins, can pass array of fully qualified plugin namespaces */, initialContext?: any): any;
+	static setInitialContext(id: string, initialContext: any): void;
+	/**
+	 * Define usage of non-official Capacitor Plugins via Android package names
+	 * @param plugins list of non-official Capacitor package names
+	 */
+	static setAndroidPlugins(plugins: Array<string>): void;
+	/**
+	 * Send a message to any web portal via publishing a topic (aka. event)
+	 * @param topic name of topic/event
+	 * @param data payload to send
+	 */
+	static publishTopic(topic: string, data?: any): void;
+	/**
+	 * Listen to any message sent from any web portal via subscribing to the topic (aka. event)
+	 * @param topic name of topic/event
+	 * @param callback method which is invoked everytime a message is sent via the topic
+	 * @returns subscription id used to unsubscribe later
+	 */
+	static subscribeToTopic(topic: string, callback: (data?: any) => void): number;
+	/**
+	 * Unsubscribe from any topic (aka. event)
+	 * @param topic name of topic/event
+	 * @param subscriptionId subscription id
+	 */
+	static unsubscribeFromTopic(topic: string, subscriptionId: number): void;
 }
 export declare class IonicPortal extends IonicPortalCommon {}
