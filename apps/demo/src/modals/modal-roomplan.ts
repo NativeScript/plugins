5ec96f0002e94bc461f4422d5c58dfa702cac7f3
Nathan Walker <walkerrunpdx@gmail.com>
2022-08-06 08:50:43 -0700

    chore: cleanup

--- apps/demo/src/modals/modal-roomplan.ts
+++ apps/demo/src/modals/modal-roomplan.ts
@@ -4,7 +4,7 @@ import { shareText } from '@nativescript/social-share';
 
 export function shownModally(args: EventData) {
 	const page = <Page>args.object;
-	page.bindingContext = new DemoModel(page);
+	page.bindingContext = new RoomPlanModel(page);
 }
 
 let roomCaptureView: RoomCaptureView;
@@ -15,7 +15,7 @@ export function loadedRoomCaptureView(args) {
 	console.log('roomCaptureView.start:', roomCaptureView.start);
 }
 
-export class DemoModel extends Observable {
+export class RoomPlanModel extends Observable {
 	scanning = true;
 
 	constructor(public page: Page) {
@@ -25,27 +25,19 @@ export class DemoModel extends Observable {
 	cancel() {
 		this.page.closeModal();
 	}
-	done() {
-		if (this.scanning) {
-			roomCaptureView.stop();
-			this.scanning = false;
-			this.notifyPropertyChange('scanning', this.scanning);
-		} else {
-			Dialogs.prompt({
-				title: 'Save 3D Room Model?',
-				message: 'Please type the name for the model...',
-				defaultText: 'my-room',
-				okButtonText: 'Save',
-				cancelButtonText: 'Cancel',
-			}).then((value) => {
-				if (value.result && value.text) {
-					roomCaptureView.export(path.join(knownFolders.documents().path, `${value.text}.usdz`), (outputPath) => {
-						this.page.closeModal(outputPath);
-					});
-				} else {
-					this.page.closeModal();
-				}
-			});
-		}
+
+	save() {
+		Dialogs.prompt({
+			title: 'Save 3D Room Model?',
+			message: 'Please type the name for the model...',
+			defaultText: 'my-room',
+			okButtonText: 'Save',
+			cancelButtonText: 'Cancel',
+		}).then((value) => {
+			if (value.result && value.text) {
+			} else {
+				this.page.closeModal();
+			}
+		});
 	}
 }
