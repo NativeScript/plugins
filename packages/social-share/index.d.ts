07734f15915a5a973abc1ada6abd6d6b5b5b6647
Eduardo Speroni <edusperoni@users.noreply.github.com>
2022-11-29 21:48:13 -0300

    feat(social-share): allow PNG image format (#367)

--- packages/social-share/index.d.ts
+++ packages/social-share/index.d.ts
@@ -1,12 +1,36 @@
 import { File, ImageSource } from '@nativescript/core';
 
+export interface ShareImageOptions {
+	/**
+	 * The subject of the share *** ANDROID ONLY ***
+	 */
+	subject?: string;
+	/**
+	 * Caption to share alongside the image
+	 */
+	caption?: string;
+	/**
+	 * The generated image format *** ANDROID ONLY ***
+	 * @default 'jpg'
+	 */
+	fileFormat?: 'png' | 'jpg';
+}
+
+/**
+ * Share an image.
+ * @param {ImageSource} image - The image to share.
+ * @param {ShareImageOptions} [options] - The options for the share
+ */
+export function shareImage(image: ImageSource, options?: ShareImageOptions);
+
 /**
  * Share an image.
  * @param {ImageSource} image - The image to share.
  * @param {string} [subject] - The subject of the share *** ANDROID ONLY ***
  * @param {string} [caption] - Caption to share alongside the image
+ * @deprecated
  */
-export function shareImage(image?: ImageSource, subject?: string, caption?: string);
+export function shareImage(image: ImageSource, subject?: string, caption?: string);
 
 /**
  * Share text.
