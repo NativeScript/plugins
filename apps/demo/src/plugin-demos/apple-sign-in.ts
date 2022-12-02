74cc8c83c22296b6dd9b0b40ab1ca0f256355e3e
Nathan Walker <walkerrunpdx@gmail.com>
2022-12-02 10:19:22 -0800

    chore: add apple-sign-in demo

--- apps/demo/src/plugin-demos/apple-sign-in.ts
+++ apps/demo/src/plugin-demos/apple-sign-in.ts
@@ -1,15 +1,10 @@
 import { Observable, EventData, Page } from '@nativescript/core';
 import { DemoSharedAppleSignIn } from '@demo/shared';
-import { } from '@nativescript/apple-sign-in';
+import { SignIn } from '@nativescript/apple-sign-in';
 
 export function navigatingTo(args: EventData) {
 	const page = <Page>args.object;
 	page.bindingContext = new DemoModel();
 }
 
-
-export class DemoModel extends DemoSharedAppleSignIn {
-	login(args) {
-		console.log('login');
-	}
-}
+export class DemoModel extends DemoSharedAppleSignIn {}
