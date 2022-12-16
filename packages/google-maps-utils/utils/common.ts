0922e78d18b8ba8b86480cdd61173a1404b81221
Dylan Llewellyn <46717769+herefishyfish@users.noreply.github.com>
2022-12-16 17:55:35 +0800

    fix(google-map-utils): mixin

--- packages/google-maps-utils/utils/common.ts
+++ packages/google-maps-utils/utils/common.ts
@@ -1,6 +1,79 @@
 import { Color } from '@nativescript/core';
 import { IGradient } from '..';
 
+export function applyMixins(
+	derivedCtor: any,
+	baseCtors: any[],
+	options?: {
+		after?: boolean;
+		override?: boolean;
+		omit?: (string | symbol)[];
+	}
+) {
+	const omits = options && options.omit ? options.omit : [];
+	baseCtors.forEach((baseCtor) => {
+		Object.getOwnPropertyNames(baseCtor.prototype).forEach((name) => {
+			if (omits.indexOf(name) !== -1) {
+				return;
+			}
+			const descriptor = Object.getOwnPropertyDescriptor(baseCtor.prototype, name);
+
+			if (name === 'constructor') return;
+			if (descriptor && (descriptor.get || descriptor.set)) {
+				Object.defineProperty(derivedCtor.prototype, name, descriptor);
+			} else {
+				const oldImpl = derivedCtor.prototype[name];
+				if (!oldImpl) {
+					derivedCtor.prototype[name] = baseCtor.prototype[name];
+				} else {
+					derivedCtor.prototype[name] = function (...args) {
+						if (options) {
+							if (!!options.override) {
+								return baseCtor.prototype[name].apply(this, args);
+							} else if (!!options.after) {
+								oldImpl.apply(this, args);
+								return baseCtor.prototype[name].apply(this, args);
+							} else {
+								baseCtor.prototype[name].apply(this, args);
+								return oldImpl.apply(this, args);
+							}
+						} else {
+							baseCtor.prototype[name].apply(this, args);
+							return oldImpl.apply(this, args);
+						}
+					};
+				}
+			}
+		});
+		Object.getOwnPropertySymbols(baseCtor.prototype).forEach((symbol) => {
+			if (omits.indexOf(symbol) !== -1) {
+				return;
+			}
+			const oldImpl: Function = derivedCtor.prototype[symbol];
+			if (!oldImpl) {
+				derivedCtor.prototype[symbol] = baseCtor.prototype[symbol];
+			} else {
+				derivedCtor.prototype[symbol] = function (...args) {
+					if (options) {
+						if (!!options.override) {
+							return baseCtor.prototype[symbol].apply(this, args);
+						} else if (!!options.after) {
+							oldImpl.apply(this, args);
+							return baseCtor.prototype[symbol].apply(this, args);
+						} else {
+							baseCtor.prototype[symbol].apply(this, args);
+							return oldImpl.apply(this, args);
+						}
+					} else {
+						baseCtor.prototype[symbol].apply(this, args);
+						return oldImpl.apply(this, args);
+					}
+				};
+			}
+		});
+	});
+}
+
 export function intoNativeColor(color: Color | string) {
 	if (color instanceof Color) {
 		return global.isAndroid ? color.android : color.ios;
