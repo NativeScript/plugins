895600165effbc10e6c28a058443a647cf0bd1e1
Kefah <kefah.bader@gmail.com>
2023-01-02 21:49:45 +0400

    Update demo

--- apps/demo/src/plugin-demos/picker.ts
+++ apps/demo/src/plugin-demos/picker.ts
@@ -5,6 +5,7 @@ import { PickerField } from '@nativescript/picker';
 export function navigatingTo(args: EventData) {
 	const page = <Page>args.object;
 	page.bindingContext = new DemoModel();
+	page.bindingContext.set('enableFilterText', 'Enable Filter');
 }
 
 export class DemoModel extends DemoSharedPicker {}
@@ -19,6 +20,17 @@ export function showPicker(args: EventData) {
         closePicker(args)
     }, 3000)
     */
+}
+
+let showFilter = false;
+export function toggleFilter(args: EventData) {
+	const view: View = args.object as View;
+	console.error('enableFilter', showFilter);
+	showFilter = !showFilter;
+	view.page.bindingContext.set('enableFilter', showFilter);
+	view.page.bindingContext.set('enableFilterText', showFilter ? 'Hide Filter' : 'Enable Filter');
+	const picker: PickerField = view.page.getViewById('picker');
+	picker.showPicker();
 }
 
 function closePicker(args: EventData) {
