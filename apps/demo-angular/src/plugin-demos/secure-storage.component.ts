e7c52ab662fb959450b319b6936cdf5a6a3cf668
George Grigalashvili <grigalaa@gmail.com>
2023-06-15 21:43:54 +0200

    feat: secure-storage - add angular demo

--- apps/demo-angular/src/plugin-demos/secure-storage.component.ts
+++ apps/demo-angular/src/plugin-demos/secure-storage.component.ts
@@ -1,10 +1,10 @@
 import { Component, NgZone } from '@angular/core';
 import { DemoSharedSecureStorage } from '@demo/shared';
-import {} from '@nativescript/secure-storage';
 
 @Component({
 	selector: 'demo-secure-storage',
 	templateUrl: 'secure-storage.component.html',
+	styleUrls: ['secure-storage.component.scss'],
 })
 export class SecureStorageComponent {
 	demoShared: DemoSharedSecureStorage;
