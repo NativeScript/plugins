c0308eb75f3e1811b3f36c833aa989e35b27a762
Paul Staskiewicz <staskiewicz@evemo.de>
2023-08-25 11:48:22 +0200

    Fix build

--- packages/google-maps/angular/index.ts
+++ packages/google-maps/angular/index.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { registerElement } from '@nativescript/angular';
-import { MapView } from '@nativescript/google-maps';
+import { MapView } from '@rebootmobility/google-maps';
 import { MapViewDirective } from './map-view-directive';
 
 export * from './map-view-directive';
